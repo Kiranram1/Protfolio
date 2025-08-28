881ccc37a3dc0156ea515976de079ff8d32e6313
Kiranram1 <kiranhr4104@gmail.com>
2025-08-28 14:28:12 +0530

    Add updated project files

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+import dynamic from "next/dynamic";
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import RecentProjects from "@/components/RecentProjects";
